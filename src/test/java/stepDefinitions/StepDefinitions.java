f4bbd4e31e56da5f151725954030fdfbd0292cb8
SurajBirajdar <78243934+SurajBirajdar@users.noreply.github.com>
2024-11-01 17:00:13 +0530

    Added TestRunner to execute test scenarios

---
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -0,0 +1,28 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+    @Given("Add place payload")
+    public void add_place_payload() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user calls {string} with POST http request")
+    public void user_calls_with_post_http_request(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("API call gets success with {int} status code")
+    public void api_call_gets_success_with_status_code(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("{string} in response body is {string}")
+    public void in_response_body_is(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
